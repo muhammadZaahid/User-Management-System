23a53a3ed49082b9a42e1c9d3e9714031bb2d544
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-07 21:27:07 +0700

    First Commit

---
+++ src/test/java/com/zaahid/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaahid.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
