da07a5031c36cd9fbc5ca795da7292b48e90c655
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-07 22:20:44 +0700

    Set db collection

--- src/main/java/com/zaahid/usermanagement/model/User.java
+++ src/main/java/com/zaahid/usermanagement/model/User.java
@@ -1,5 +1,16 @@
 package com.zaahid.usermanagement.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+@Document(collection = "User")
 public class User {
     
 }
