7057c2d2b16a94888adb98951bf397070522581e
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-08 11:02:25 +0700

    add user function

--- src/main/java/com/zaahid/usermanagement/resource/UserController.java
+++ src/main/java/com/zaahid/usermanagement/resource/UserController.java
@@ -1,12 +1,21 @@
 package com.zaahid.usermanagement.resource;
 
+import com.zaahid.usermanagement.model.User;
 import com.zaahid.usermanagement.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class UserController {
 
    @Autowired
    private UserRepository repository; 
+
+   @PostMapping("/addUser")
+   public String saveUser(@RequestBody User user){
+        repository.save(user);
+        return "Added user with id : " + user.getId();
+   }
     
 }
