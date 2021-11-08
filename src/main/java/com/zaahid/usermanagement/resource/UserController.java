d5959a79cc9c271a9b25a9a53a87365a851c0a3a
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-08 10:52:26 +0700

    call user repo

--- src/main/java/com/zaahid/usermanagement/resource/UserController.java
+++ src/main/java/com/zaahid/usermanagement/resource/UserController.java
@@ -1,5 +1,12 @@
 package com.zaahid.usermanagement.resource;
 
+import com.zaahid.usermanagement.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserController {
+
+   @Autowired
+   private UserRepository repository; 
     
 }
